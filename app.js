c434a607697acc0054612f41125ebbea92a23364
unknown <singh.avvi98@gmail.com>
2021-01-10 16:22:33 +1100

    Inserted seed file with mock data

--- app.js
+++ app.js
@@ -6,4 +6,14 @@ const connection = mysql.createConnection({
     user: 'root',
     password: 'rootroot',
     database: 'employee_tracker'
-    });
+});
+
+const pool = mysql.createPool(connection);
+
+connection.connect((err) => {
+    if (err) {
+        console.log('could not connect: ' + err.stack)
+    }
+
+    console.log('connected as id ' + connection.threadId);
+});
---
+++ seed.sql
@@ -0,0 +1,34 @@
+USE employee_tracker;
+
+INSERT INTO department (name)
+VALUES
+('Finance'),
+('Operations'),
+('Product Development');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('CFO', 120000, 1),
+('COO', 120000, 2),
+('Head of Product Development', 120000, 3),
+('CEO', 135000, NULL);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Mark', 'Waugh', 4, NULL),
+('Stephen', 'Larkham', 1, 1),
+('George', 'Gregan', 2, 1),
+('David', 'Pocock', 3, 3);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
+
+SELECT 
+first_name,
+last_name,
+title,
+salary
+FROM role
+LEFT JOIN employee
+ON employee.role_id = role.id;
